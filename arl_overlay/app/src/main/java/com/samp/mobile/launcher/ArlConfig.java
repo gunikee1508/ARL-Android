9355579157577102792f3310444da069132594af
gunikee1508 <57869814+gunikee1508@users.noreply.github.com>
2026-09-16 10:32:01 -0300

    Add ARL launcher config

---
+++ arl_overlay/app/src/main/java/com/samp/mobile/launcher/ArlConfig.java
@@ -0,0 +1,14 @@
+package com.samp.mobile.launcher;
+
+public final class ArlConfig {
+    private ArlConfig() {}
+    public static final String DEFAULT_HOST = "play.arl-samprpg.site";
+    public static final int DEFAULT_PORT = 7777;
+    public static final String CLIENT_VERSION = "0.3.7-R3";
+    public static final String REMOTE_CONFIG =
+        "https://play.arl-samprpg.site/android/launcher.json";
+    public static final String DISCORD = "https://discord.gg/7X6swkqHDt";
+    public static final String INSTAGRAM =
+        "https://www.instagram.com/amazingreallifesamp";
+    public static final String FORUM = "https://arl-samprpg.forumeiros.com";
+}
